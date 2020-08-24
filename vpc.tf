9625470465ad2af4295a3e089795591a5000861b
julie rudolph <rudolphja@yahoo.com>
2020-08-24 19:42:54 +0000

    Added VPC module

---
+++ vpc.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.2.1.0/24"
+
+#  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-1"]
+  private_subnets = ["10.2.1.0/28", "10.2.1.16/28"]
+  public_subnets  = ["10.2.1.128/28"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
